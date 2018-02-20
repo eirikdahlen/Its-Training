58a61343026fd3b8481d8e1c214f6489a61edd49
Frikk Hald Andersen <frikkhaldandersen@dhcp-10-22-8-144.wlan.ntnu.no>
2018-02-20 09:03:35 +0100

    #3 Implementere User-klassen

---
+++ tdt4140-gr1802/app.core/src/main/java/tdt4140/gr1802/app/core/User.java
@@ -0,0 +1,24 @@
+package tdt4140.gr1802.app.core;
+
+public abstract class User {
+	
+	protected String name ;
+		
+	protected String username ;
+	
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+	
+	
+
+}
