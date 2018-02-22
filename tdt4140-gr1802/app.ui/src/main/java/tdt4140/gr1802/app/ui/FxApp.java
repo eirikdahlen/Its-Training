879a9ead5e8c98cc50051bfdcaaa5d90e29263ca
andrekm <andrekm@stud.ntnu.no>
2018-02-22 15:37:24 +0100

    #1 #2 #3 Implemented login screen and sign up screen

--- tdt4140-gr1802/app.ui/src/main/java/tdt4140/gr1802/app/ui/FxApp.java
+++ tdt4140-gr1802/app.ui/src/main/java/tdt4140/gr1802/app/ui/FxApp.java
@@ -7,13 +7,17 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class FxApp extends Application {
+	
+	Stage window; 
+	Scene loginScreen, signUpScreen; 
+	
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FxApp.fxml"));
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        Scene scene = new Scene(root,800,600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
--- tdt4140-gr1802/app.ui/src/main/java/tdt4140/gr1802/app/ui/FxAppController.java
+++
@@ -1,4 +0,0 @@
-package tdt4140.gr1802.app.ui;
-
-public class FxAppController {
-}
