abc96918cb9a51a35063c1a7f53f833092e9de8c
Karoline Bonnerud <kbonnerud@gmail.com>
2018-02-25 19:59:20 +0100

    #1 #2 Check valid input in signup controller

--- tdt4140-gr1802/app.ui/src/main/java/tdt4140/gr1802/app/ui/SignUpScreenController.java
+++ tdt4140-gr1802/app.ui/src/main/java/tdt4140/gr1802/app/ui/SignUpScreenController.java
@@ -7,10 +7,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import tdt4140.gr1802.app.core.Database;
 
 import java.awt.TextArea;
 import java.io.BufferedReader;
@@ -28,47 +30,39 @@ import java.util.List;
 import javafx.event.ActionEvent;
 
 
-public class SignUpScreenController {
-	
+public class SignUpScreenController{
 	@FXML
 	private Text txtName; 
-	
 	@FXML
 	private Text txtUsername;
-	
 	@FXML
 	private Text txtPassword;
-	
 	@FXML
 	private Text txtRetypePassword;
-	
 	@FXML
 	private Text txtAthOrCoa;
-	
 	@FXML
 	private TextField txtFieldName; 
-	
 	@FXML
 	private TextField txtFieldUsername;
-	
 	@FXML
 	private TextField txtFieldPassword;
-	
 	@FXML
-	private TextField txtPasswordRetype;
-	
+	private TextField txtFieldPasswordRetype;
 	@FXML
 	private Button btbsignUp_back; 
-	
 	@FXML
 	private Button BackToLoginFromSignUp; 
-	
 	@FXML
-	private ChoiceBox choiceBoxAthleteOrCoach;
+	private RadioButton radioAthlete;
+	@FXML
+	private RadioButton radioCoach;
+
+	Database db = new Database();
 	
 	
 	public boolean checkValidName() {
-		// Check if name only contains lettes
+		// Check if name only contains letters
 		if (txtName.getText().matches("[a-zA-Z]")) {
 			return true;
 		} else {
@@ -77,9 +71,34 @@ public class SignUpScreenController {
 	}
 	
 	public boolean checkUserName() {
-		return false; 
+		String typedUserName = txtFieldUsername.getText(); 
+		
+		if (db.coachUsernameExists(typedUserName) || db.athleteUsernameExists(typedUserName)) {
+			// Username exits in database
+			System.out.println("Username already in database");
+			return false;
+		} else {
+			return false;
+		}
 	}
 	
+	public boolean checkPasswordLenght() {
+		String typedPassword = txtFieldPassword.getText();
+		
+		if (typedPassword.length() <= 4) {
+			System.out.println("Password shorter than 4 chars");
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public boolean checkEqualPasswords() {
+		return txtFieldPassword.getText().equals(txtFieldPasswordRetype.getText());
+	}
+	
+	private void radioAthleteButtonPressed() {radioCoach.setSelected(false); radioAthlete.setSelected(true);}
+	private void coachAthleteButtonPressed() {radioCoach.setSelected(true); radioAthlete.setSelected(false);}
 	
 	
 	
@@ -127,5 +146,14 @@ public class SignUpScreenController {
 		window.show();
 	}
 	
+
+	
+	
+	public static void main(String[] args) {
+	
+		System.out.println("hei");
+		
+		
+	}
 	
 }
