1d640c01fcef638a8b11c9fad2961fab1454f1d0
Karoline Bonnerud <kbonnerud@gmail.com>
2018-03-02 11:19:06 +0100

    #3 Implemented LogIn-class

---
+++ tdt4140-gr1802/app.core/src/main/java/tdt4140/gr1802/app/core/LogIn.java
@@ -0,0 +1,37 @@
+package tdt4140.gr1802.app.core;
+
+public class LogIn {
+	
+	User inloggedUser;
+	
+	Database db = new Database();
+	
+	public LogIn(String username, String password) {
+		if ( checkUsernameAthlete(username) ||
+				checkUsernameMatchPassword(username, password)) {
+				// Athlete-login
+				inloggedUser = db.getAthlete(username);
+		} else if (
+				checkUsernameCoach(username) ||
+				checkUsernameMatchPassword(username, password)) {
+				// Coach-login
+				inloggedUser = db.getCoach(username);
+		}
+		
+	}
+	
+	public boolean checkUsernameAthlete(String username) {
+		return db.athleteUsernameExists(username);
+	}
+	
+	public boolean checkUsernameCoach(String username) {
+		return db.coachUsernameExists(username);
+	}
+	
+	public boolean checkUsernameMatchPassword(String username, String password) {
+		return db.getPassword(username).equals(password);
+	}
+	
+	
+
+}
