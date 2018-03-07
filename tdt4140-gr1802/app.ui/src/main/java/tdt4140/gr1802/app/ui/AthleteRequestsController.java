1b03f6a500936505fcf7af7af6a386bf25df60d6
Ida Merete Enholm <ida.enholm@me.com>
2018-03-07 09:54:07 +0100

    #26 #21 UI for Coach seeing Athlete-requests and approve/decline the request

---
+++ tdt4140-gr1802/app.ui/src/main/java/tdt4140/gr1802/app/ui/AthleteRequestsController.java
@@ -0,0 +1,116 @@
+package tdt4140.gr1802.app.ui;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import tdt4140.gr1802.app.core.Athlete;
+import tdt4140.gr1802.app.core.Coach;
+import tdt4140.gr1802.app.core.Database;
+
+public class AthleteRequestsController {
+	
+	private Database database = new Database();
+	private Coach coach = database.getCoach("Petter74");
+	
+	@FXML
+	private TableView<Athlete> tableView;
+	
+	@FXML
+	private TableColumn<Athlete, String> usernameColumn;
+	
+	@FXML
+	private TableColumn<Athlete, String> nameColumn;
+	
+	@FXML
+	private Button btSeeAthletes;
+	
+	@FXML
+	private Button btAthleteRequests;
+	
+	private ObservableList<Athlete> athletes = FXCollections.observableArrayList();
+	
+	// Help-method that returns an ObervableList with all the Athlete-requests
+	public ObservableList<Athlete> getAthletes(){
+		for (String uname:coach.getQueuedAthletes()) {
+			Athlete athlete = database.getAthlete(uname);
+			if (athlete != null) {
+				athletes.add(athlete);
+			}
+		}
+		return athletes;
+	}
+	
+	public void initialize() {
+		// Connect columns to right attribute
+		usernameColumn.setCellValueFactory(new PropertyValueFactory<Athlete,String>("username"));
+		nameColumn.setCellValueFactory(new PropertyValueFactory<Athlete,String>("name"));
+		
+		// Make it legal to select multiple rows
+		tableView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+		
+		// Fill table with values
+		tableView.setItems(getAthletes());
+	}
+	
+	public void acceptButton(ActionEvent event) throws IOException, RuntimeException, InvocationTargetException{
+		ObservableList<Athlete> selectedRows;
+		
+		// Gives us the rows that are selected
+		selectedRows = tableView.getSelectionModel().getSelectedItems();
+		
+		// For each Athlete selected; remove from the screen and approve
+		for (Athlete athlete : selectedRows) {
+			athletes.remove(athlete);
+			coach.approveAthlete(athlete.getUsername());
+		}
+		
+	}
+
+	public void declineButton(ActionEvent event) throws IOException, RuntimeException, InvocationTargetException{
+		ObservableList<Athlete> selectedRows;
+		
+		// Gives us the rows that are selected
+		selectedRows = tableView.getSelectionModel().getSelectedItems();
+		
+		// For each Athlete selected; remove from the screen and decline
+		for (Athlete athlete : selectedRows) {
+			athletes.remove(athlete);
+			coach.declineAthlete(athlete.getUsername());
+		}
+	}
+	
+	
+	// Side-menu buttons
+	public void clickSeeAthletes (ActionEvent event) throws IOException{
+		Parent root = FXMLLoader.load(getClass().getResource("SeeAthletes.fxml"));
+		Scene scene = new Scene(root,800,600);
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		
+		window.setScene(scene);
+		window.show();
+	}
+	
+	public void clickAthleteRequests (ActionEvent event) throws IOException{
+		Parent root = FXMLLoader.load(getClass().getResource("AthleteRequests.fxml"));
+		Scene scene = new Scene(root,800,600);
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		
+		window.setScene(scene);
+		window.show();
+	}
+
+}
