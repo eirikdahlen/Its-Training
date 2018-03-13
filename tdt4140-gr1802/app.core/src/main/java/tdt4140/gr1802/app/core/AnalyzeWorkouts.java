363c12c9be43274e87ffb486a6414e1e37e01f7d
petter <petter@192.168.38.105>
2018-03-13 22:25:28 +0100

    #63 implemented analyzeWorkout

---
+++ tdt4140-gr1802/app.core/src/main/java/tdt4140/gr1802/app/core/AnalyzeWorkouts.java
@@ -0,0 +1,11 @@
+package tdt4140.gr1802.app.core;
+
+public class AnalyzeWorkouts {
+	//class for analysis-methods for a single workout
+	
+	
+	
+	
+	
+	
+}
---
+++ tdt4140-gr1802/app.core/src/test/java/tdt4140/gr1802/app/core/AnalyzeWorkoutsTest.java
@@ -0,0 +1,5 @@
+package tdt4140.gr1802.app.core;
+
+public class AnalyzeWorkoutsTest {
+
+}
