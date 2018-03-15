ee3fe4c9c1b07f57b3910720aeccec9cd6cec69e
Ida Merete Enholm <ida.enholm@me.com>
2018-03-15 12:13:15 +0100

    #68 Added PieChart and time in pulse zones

--- tdt4140-gr1802/app.ui/src/main/java/tdt4140/gr1802/app/ui/HomeScreenAthleteController.java
+++ tdt4140-gr1802/app.ui/src/main/java/tdt4140/gr1802/app/ui/HomeScreenAthleteController.java
@@ -1,7 +1,10 @@
 package tdt4140.gr1802.app.ui;
 
 import java.io.IOException;
+import java.util.List;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,9 +12,12 @@ import javafx.fxml.LoadException;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import tdt4140.gr1802.app.core.AnalyzeWorkout;
+import tdt4140.gr1802.app.core.AnalyzeWorkouts;
 import tdt4140.gr1802.app.core.App;
 import tdt4140.gr1802.app.core.Athlete;
 import tdt4140.gr1802.app.core.Coach;
@@ -36,10 +42,40 @@ public class HomeScreenAthleteController {
 	@FXML
 	private Label txtLabelUsername;
 	
+	@FXML
+	private Label lowLabel;
+	
+	@FXML
+	private Label moderateLabel;
+	
+	@FXML
+	private Label highLabel;
+	
+	@FXML
+	private PieChart zoneChart;
+	
+	ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+	private AnalyzeWorkouts analyzer = new AnalyzeWorkouts();
+	
 	public void initialize() {
 		App.updateAthlete();
 		this.athlete = App.getAthlete();
 		this.txtLabelUsername.setText(this.athlete.getUsername());
+		
+		List<Integer> timeInZones = analyzer.getTimeInHRZones(athlete.getAllWorkouts());
+		
+		lowLabel.setText(String.valueOf(timeInZones.get(0)) + " min");
+		moderateLabel.setText(String.valueOf(timeInZones.get(1)) + " min");
+		highLabel.setText(String.valueOf(timeInZones.get(2)) + " min");
+		
+		// Fill PieChart with pulsZones
+		System.out.println(timeInZones);
+		pieChartData.add(new PieChart.Data("Low", timeInZones.get(0)));
+		pieChartData.add(new PieChart.Data("Moderate", timeInZones.get(1)));
+		pieChartData.add(new PieChart.Data("High", timeInZones.get(2)));
+		System.out.println(pieChartData);
+		zoneChart.setData(pieChartData);
+		zoneChart.setTitle("Total time in pulse zones");
 	}
 	
 	// Side-menu buttons
