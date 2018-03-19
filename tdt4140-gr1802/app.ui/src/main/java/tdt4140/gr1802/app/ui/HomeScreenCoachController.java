9e6dafe9d030f005a072b484845602a9df85dc17
karolbon <karolbon@stud.ntnu.no>
2018-03-19 13:03:19 +0100

    #69 removed unuses import

--- tdt4140-gr1802/app.ui/src/main/java/tdt4140/gr1802/app/ui/HomeScreenCoachController.java
+++ tdt4140-gr1802/app.ui/src/main/java/tdt4140/gr1802/app/ui/HomeScreenCoachController.java
@@ -15,7 +15,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
