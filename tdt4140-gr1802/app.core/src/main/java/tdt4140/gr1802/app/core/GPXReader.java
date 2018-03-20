bfc19cd684829ff3d75de0c93bf4d41b9e93a915
Frikk Hald Andersen <frikkhaldandersen@dhcp-10-22-10-92.wlan.ntnu.no>
2018-03-20 15:39:37 +0100

    #58

--- tdt4140-gr1802/app.core/src/main/java/tdt4140/gr1802/app/core/GPXReader.java
+++ tdt4140-gr1802/app.core/src/main/java/tdt4140/gr1802/app/core/GPXReader.java
@@ -2,18 +2,15 @@ package tdt4140.gr1802.app.core;
 
 import java.util.List;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import io.jenetics.jpx.GPX;
-import io.jenetics.jpx.Route;
+
 import io.jenetics.jpx.Track;
 import io.jenetics.jpx.TrackSegment;
 import io.jenetics.jpx.WayPoint;
-import com.lynden.gmapsfx.javascript.object.LatLong;
+
 
 
 public class GPXReader {
