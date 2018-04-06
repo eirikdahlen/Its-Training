68daa9466322d623d133f1dc5cc6476acc556f20
Frikk Hald Andersen <frikkhaldandersen@dhcp-10-22-9-23.wlan.ntnu.no>
2018-04-06 14:34:21 +0200

    #93 #94 Made CSVreader for sleepdata, and methods for accessing it  in athelte and database

---
+++ tdt4140-gr1802/app.core/src/main/java/tdt4140/gr1802/app/core/CSVsleep.java
@@ -0,0 +1,73 @@
+package tdt4140.gr1802.app.core;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CSVsleep {
+	
+	private URL filePath;
+	
+	public static final int timeStep = 10;
+
+	
+	public CSVsleep (URL filePath) {
+		this.filePath = filePath ;
+	}
+	
+	public List<List<String>> getSleepData() {
+
+		URL csvFile = this.filePath;
+        
+		BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ";";
+        
+        try {
+        		List<List<String>> sleepdata = new ArrayList<List<String>>(); 
+        		int i = 0;
+
+            br = new BufferedReader(new InputStreamReader(filePath.openStream()));
+            while ((line = br.readLine()) != null) {
+            		if (line.isEmpty() || i == 0 || i == 1) {
+            			System.out.println("Empty");
+            		} else {
+                // use comma as separator
+            			String[] linje = line.split(cvsSplitBy);
+            			String date = linje[1].substring(0,10);
+            			List <String> day = Arrays.asList(date, linje[2], linje[3]);
+            			sleepdata.add(day);
+            			/*for (String e: day) {
+            				System.out.println(e);
+            			}*/
+            		}
+                //System.out.println("kommer hit");
+                i++;
+            }
+            
+            //returns array
+            return sleepdata;
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    return null;
+	}
+	
+	
+}
